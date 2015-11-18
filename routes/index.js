7daaf8163f98a1085fa418e8c80a4dc8e3465574
Vivek Mano <vivekmano@gmail.com>
2015-11-18 05:02:55 +0000

    Include stepify jquery plugin for stepping from fieldset to fieldset

--- routes/index.js
+++ routes/index.js
@@ -9,23 +9,24 @@ router.get('/', function(req, res, next) {
 router.post('/', function (req, res){
 	console.log('in the post function...');
 	var body = req.body;
-	//available variables from form:
-	/*
-	headline
-	problem
-	solution
-	f1
-	b1
-	...
-	f5
-	b5
-	offer
-	
-	
-	*/
-	
-	
-	var htmlContent = "Name: " + body.firstname;
+	var htmlContent = "Name: "+body.firstname+" "+body.lastname+" <br>\
+					   Email: " +body.email+"<br>\
+					   URL: "+body.url+"<br><br>\
+					   Headline: "+body.headline+"<br><br>\
+					   Problem: "+body.problem+"<br>\
+					   Solution: "+body.solution+"<br><br>\
+					   Feature: "+body.f1+"<br>\
+					   Benefit: "+body.b1+"<br><br>\
+					   Feature: "+body.f2+"<br>\
+					   Benefit: "+body.b2+"<br><br>\
+					   Feature: "+body.f3+"<br>\
+					   Benefit: "+body.b3+"<br><br>\
+					   Feature: "+body.f4+"<br>\
+					   Benefit: "+body.b4+"<br><br>\
+					   Feature: "+body.f5+"<br>\
+					   Benefit: "+body.b5+"<br><br>\
+					   Offer: "+body.offer+"<br>";
+	//console.log(htmlContent);				   
 	var customMail = new CustomMail(htmlContent);
 	
 	customMail.send();
