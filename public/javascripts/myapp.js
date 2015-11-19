6e6a0986624639ff2d23e158498d1ec0c097903f
Vivek Mano <vivekmano@gmail.com>
2015-11-19 21:06:00 +0000

    Style the next/prev buttons, disable the navbar to avoid confusion

--- public/javascripts/myapp.js
+++ public/javascripts/myapp.js
@@ -2,23 +2,23 @@ $('.step').stepify({
    distribution:[1,1,1,1,1],
    nextHooks : {
 		0 : [ 
-				function(){ $('.active').removeClass('active').next().addClass('active');},
+				function(){ $('.active').removeClass('active').addClass('done').next().addClass('active');},
 				function ($stepContainer){ return true;} 
 			],
 		1 : [ 
-				function(){ $('.active').removeClass('active').next().addClass('active');},
+				function(){ $('.active').removeClass('active').addClass('done').next().addClass('active');},
 				function ($stepContainer){ return true;} 
 			],
 		2 : [ 
-				function(){ $('.active').removeClass('active').next().addClass('active');},
+				function(){ $('.active').removeClass('active').addClass('done').next().addClass('active');},
 				function ($stepContainer){ return true;} 
 			],
 		3 : [ 
-				function(){ $('.active').removeClass('active').next().addClass('active');},
+				function(){ $('.active').removeClass('active').addClass('done').next().addClass('active');},
 				function ($stepContainer){ return true;} 
 			],
 		4 : [ 
-				function(){ $('.active').removeClass('active').next().addClass('active');},
+				function(){ $('.active').removeClass('active').addClass('done').next().addClass('active');},
 				function ($stepContainer){ return true;} 
 			]
 	},
@@ -41,4 +41,15 @@ $('.step').stepify({
 				function ($stepContainer){ return true;} 
 			]
 	}
+// 	,nextBtn : {
+// 				text : 'Next &gt;',
+// 				cssClass : 'btn-primary'
+// 			}
+
 });
+
+//add onclick handler for the navigation tabs
+//when triggered, need to:
+//1) move to correct section
+//2) make sure that the buttons below are correct
+//3) update the navbar
